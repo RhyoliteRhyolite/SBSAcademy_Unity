33e909de56bb9beb60ed909c7f709dde80f24908
유문암 <rhyolite199@gmail.com>
2024-08-27 19:43:47 +0900

    class13th

---
+++ Assets/Class11th/Skripts/Dragon.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    [SerializeField] AudioClip audioClip;
+    public void Attack()
+    {
+        SoundManager.instance.Listen(audioClip);
+    }
+}
