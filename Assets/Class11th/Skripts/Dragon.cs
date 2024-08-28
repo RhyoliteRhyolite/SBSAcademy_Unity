c5943759e6e2c5c4039b978950379efa8c3d71ee
유문암 <rhyolite199@gmail.com>
2024-08-28 19:43:34 +0900

    class14th

--- Assets/Class11th/Skripts/Dragon.cs
+++ Assets/Class11th/Skripts/Dragon.cs
@@ -2,9 +2,30 @@ using UnityEngine;
 
 public class Dragon : MonoBehaviour
 {
+    private int count = 0;
     [SerializeField] AudioClip audioClip;
+    [SerializeField] Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void Attack()
     {
+        if (count >= 10)
+        {
+            animator.SetTrigger("Die");
+            return;
+        }
+
+        count++;
+
         SoundManager.instance.Listen(audioClip);
+    }
+
+    public void Release()
+    {
+        Destroy(gameObject);
     }
 }
